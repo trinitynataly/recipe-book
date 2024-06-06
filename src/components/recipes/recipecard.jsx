a4a7baee4363f08c128edace9f0f050ae204e4e4
trinitynataly <trinity_nataly@gmail.com>
2024-06-06 15:06:06 +1000

    fix

--- src/components/recipes/recipecard.jsx
+++ src/components/recipes/recipecard.jsx
@@ -148,7 +148,6 @@ const RecipeCard = ({ recipe }) => {
               objectFit="cover"
               sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
               quality={75}
-              priority={true}
             />
           </div>
         </Link>
